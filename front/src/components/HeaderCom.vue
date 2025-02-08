eb0f78abdd990c497852782c280cf856b225f86f
ChangHyukGod <pgc10000@naver.com>
2025-02-08 23:13:02 +0900

    25/02/08 HeaderCom,HomeView
    헤더, 메인화면 뼈대 제작

--- front/src/components/HeaderCom.vue
+++ front/src/components/HeaderCom.vue
@@ -1,9 +1,31 @@
 <template>
-    <div>
-        <h1>Header</h1>
-        <router-link to="/">Main</router-link>&nbsp;
-        <router-link to="/character">Character</router-link>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">LoA24</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/character">Character</a>
+        </li>
+        <form class="d-flex" role="search" style="margin-left: 10px;">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success bt-size" type="submit">검색</button>
+        </form>
+      </ul>
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/signup">Signup</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
 </template>
 <script>
 export default {
@@ -11,5 +33,8 @@ export default {
 }
 </script>
 <style>
-    
+.bt-size {
+    width: 100px; /* 버튼의 고정 너비 */
+    height: 40px; /* 버튼의 고정 높이 */
+}
 </style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>
-    Main
+  <div class ="card container text-center p-3 mt-5">
+  <div class="row">
+    <div class="col-sm-9 d-flex">
+      <div class="card flex-grow-1">
+        <div class="card-body">
+          <h5 class="card-title">경매장</h5>
+          <p class="card-text"></p>
+          <img src="https://placehold.co/800x500" class="object-fit-none border rounded " alt="...">
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-3 d-flex flex-column ">
+      <div class="card flex-grow-1 mb-2">
+        <div class="card-body">
+          <h5 class="card-title">대표 캐릭터</h5>
+          <p class="card-text">로그인 후 대표 캐릭터 이미지 칸</p>
+          <img src="https://placehold.co/200x200" class="object-fit-cover border rounded" alt="...">
+        </div>
+      </div>
+      <div class="card flex-grow-1">
+        <div class="card-body">
+          <h5 class="card-title">업데이트 내역</h5>
+            <ul class="list-group ">
+              <li class="list-group-item list-group-item-action">기상술사 대폭상향 대박티비</li>
+              <li class="list-group-item list-group-item-action">건슬링어 상향안</li>
+              <li class="list-group-item list-group-item-action">모 기상 유저 에스더의 기운 득</li>
+              <li class="list-group-item list-group-item-action">환수사 너프점</li>
+              <li class="list-group-item list-group-item-action">보석 겁작 변환가능 ㄷㄷ</li>
+            </ul>
+        </div>
+      </div>
+    </div>
   </div>
+  <div class="row">
+    <div class="col">
+      <div class="card mt-2">
+        <div class="card-body">
+        <h5 class="card-title">숙제 현황(표)</h5>
+        <p class="card-text">캐릭터별 상위레이드 표시 및 클리어 여부 표시</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 <script>
 export default {

364b2f56221068122dd9f1c2c863cf538b503cb4
ChangHyukGod <pgc10000@naver.com>
2025-02-08 15:47:07 +0900

    front 뼈대 생성
    front 뼈대 생성

---
+++ front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 머리말 -->
+  <HeaderCom/>
+  <!-- 본문 -->
+  <router-view/>
+</template>
+<script>
+import HeaderCom from './components/HeaderCom.vue';
+export default {
+  components:{
+    HeaderCom,
+  }
+};
+</script>
+
+<style>
+
+</style>

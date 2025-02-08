49625fb4285ce7243341c36963a44a3265e26216
halmi-is <m0000590@naver.com>
2025-02-08 19:15:15 +0900

    헤더 메뉴_메인 추가
    페이지 이동 버튼 생성(임시)

--- front/src/components/HeaderCom.vue
+++ front/src/components/HeaderCom.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Header</h1>
+        <router-link to="/">Main</router-link>&nbsp;
         <router-link to="/character">Character</router-link>
     </div>
 </template>
